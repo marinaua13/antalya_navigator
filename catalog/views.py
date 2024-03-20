0d1f23fd0b1b1dec71c4a503082527669444921c
Marina <marina.ua13@gmail.com>
2024-03-20 15:15:06 +0200

    add "includes" to antalya_navigator; create urls in catalog with first page; create first view for index page; create first template for page index

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Author, Offer, Service
+
+
+def index(request: HttpRequest) -> HttpResponse:
+
+    num_authors = Author.objects.count()
+    num_offers = Offer.objects.count()
+    num_services = Service.objects.count()
+
+    context = {
+        "num_authors": num_authors,
+        "num_offers": num_offers,
+        "num_services": num_services,
+    }
+    return render(request, "catalog/index.html", context=context)
